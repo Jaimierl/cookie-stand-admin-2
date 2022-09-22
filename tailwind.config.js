63472ed8987b1980e7786d04e75cc990f9120094
Jae Loney <jaimieloney@gmail.com>
2022-09-22 23:40:58 +0000

    CSS

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'text-black': '#000403',
+        'header-green':'#36ca95',
+        'main-bg':'#ecfdf5',
+        'form-bg': '#6fe6b7',
+        'button':'#15b981',
+        'table-text':'#606b75',
+        'footer-green':'#15b981',
+        'footer-text':'#214244',
+      },
+    },
+  },
+  plugins: [],
+}
